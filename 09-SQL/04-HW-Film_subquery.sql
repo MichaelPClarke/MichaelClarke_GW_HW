c53bc80e5aa90ef786a54429bc0799fab303e191
michaelpclarke@gmail.com <michaelpclarke@gmail.com>
2020-06-04 00:46:57 -0400

    Update

--- 09-SQL/04-HW-Film_subquery.sql
+++ 09-SQL/04-HW-Film_subquery.sql
@@ -57,7 +57,38 @@ WHERE film_id IN
   WHERE title = 'ALTER VICTORY'
 );
 
+
+Select first_name
+	,last_name
+FROM actor
+WHERE
+	actor_id IN (
+			SELECT actor_id
+		FROM film_actor
+		WHERE
+			film_id IN (
+					SELECT film_id
+				FROM film
+				WHERE title = 'ALTER VICTORY'));
 -- Check to more sure the subquery returned correct value
 SELECT *
 FROM inventory
 WHERE film_id IN (268);
+
+
+--Jon Stephens
+SELECT title
+	FROM film
+	WHERE film_id in
+		(
+		SELECT film_id
+		FROM inventory
+		WHERE
+		inventory_id in (
+			SELECT inventory_id
+		from rental where
+			staff_id in (
+			select staff_id
+			from staff
+			where first_name = 'Jon'
+			AND last_name = 'Stephens')))
